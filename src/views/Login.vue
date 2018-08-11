b9146b5324a7af23c06a4935f2a86da4321627cc
sally168 <41235579+sally168@users.noreply.github.com>
2018-08-11 09:17:33 +0800

    登陆静态页面提交

---
+++ src/views/Login.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="login-wrap">
+    <el-form
+    class="login-form"
+    :label-position="top"
+    label-width="80px"
+    :model="formData">
+        <h2>用户登录</h2>
+        <el-form-item label="用户名">
+            <el-input v-model="formData.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码">
+            <el-input type="password" v-model="formData.password"></el-input>
+        </el-form-item>
+        <el-form-item >
+            <el-button type="primary" class="login-btn">登陆</el-button>
+        </el-form-item>
+        
+    </el-form>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      labelPosition: 'right',
+      formData: {
+        username: '',
+        password: ''
+      }
+    };
+  }
+};
+</script>
+
+<style scoped>
+ .login-wrap {
+  background-color: #324152;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.login-wrap .login-form {
+  background-color: #fff;
+  width: 400px;
+  padding: 30px;
+  border-radius: 5px;
+}
+
+.login-wrap .login-form .login-btn {
+  width: 100%;
+}
+</style>
